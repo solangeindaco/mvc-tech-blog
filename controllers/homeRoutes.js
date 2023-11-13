e22f2758086c6b0cbe2a314230ead5327b1cdadf
SolangeI <solange.indaco@gmail.com>
2023-11-13 13:25:39 -0800

    Added functionality for update a Post

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -49,6 +49,28 @@ router.get('/post/:id', async (req, res) => {
   }
 });
 
+router.get('/updatepost/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('updatepost', {
+      ...post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Use withAuth middleware to prevent access to route
 router.get('/dashboard', withAuth, async (req, res) => {
   try {
---
+++ public/js/edit-post.js
@@ -0,0 +1,29 @@
+const updateFormHandler = async (event) => {
+    event.preventDefault();
+    console.log("CLICK ON updateFormHandler");
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+      const title = document.querySelector('#post-title').value.trim();
+      const content = document.querySelector('#post-content').value.trim();
+  
+      if (title && content) {
+        const response = await fetch(`/api/posts/${id}`, {
+          method: 'PUT',
+          body: JSON.stringify({ title, content }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+  
+        if (response.ok) {
+          document.location.replace('/dashboard');
+        } else {
+          alert('Failed to update post');
+        }
+      }
+    }
+  };
+
+  document
+  .querySelector('.update-post-form')
+  .addEventListener('submit', updateFormHandler);
