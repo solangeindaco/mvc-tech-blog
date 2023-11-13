c011dc9f0024e82e4cad2f9bdcf6800b915e1097
SolangeI <solange.indaco@gmail.com>
2023-11-13 09:57:13 -0800

    Added a functionality to update a Post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -15,6 +15,25 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+// Update a Post
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update(
+      ...req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!postData) {
+      res.status(404).json({ message: 'No Post found with this id!' });
+      return;
+    }
+    res.status(200).json(postData);
+  }catch (error){
+    res.status(500).json(error);
+  }
+});
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.destroy({
