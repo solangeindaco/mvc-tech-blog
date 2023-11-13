5c4bb5d7bdf95058f046351300aeb4026e1c7f22
SolangeI <solange.indaco@gmail.com>
2023-11-13 09:01:05 -0800

    Added public, utils and views folders

---
+++ public/js/post.js
@@ -0,0 +1,30 @@
+const newCommentFormHandler = async (event) => {
+    event.preventDefault();
+    const postId = event.target.getAttribute("data-id");
+    const description = document.querySelector("#description").value;
+    
+    // Send fetch request to add a new dish
+    console.log(postId);
+    console.log(user_id);
+    console.log(description);
+    const response = await fetch(`/api/comments`, {
+      method: 'POST',
+      body: JSON.stringify({
+        description,
+        user_id,
+        post_id: postId,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    //if the comment is added,
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to add comment');
+    }
+};
+console.log("QUERY SELECTOR");
+console.log(document.querySelector(".comment-form"));
+document.querySelector(".comment-form").addEventListener("submit", newCommentFormHandler);
