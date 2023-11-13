3de3690774fa51fb6657c17bfacf36e17ab72230
SolangeI <solange.indaco@gmail.com>
2023-11-13 10:16:51 -0800

    Added updateFormHandler

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -21,6 +21,31 @@ const newFormHandler = async (event) => {
   }
 };
 
+const updateFormHandler = async (event) => {
+  event.preventDefault();
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#post-content').value.trim();
+
+    if (title && content) {
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, content }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to update post');
+      }
+    }
+  }
+};
+
 const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
